b1fe441115b110374f00110da8ac0e13ac2cd10f
Gaspar Luik <luik.gaspar@gmail.com>
2021-06-07 15:29:16 +0300

    broken

--- fnc_add.php
+++ fnc_add.php
@@ -1,57 +1,46 @@
 
 <?php
 //POST submittimise funktsioon String ja Int väärtustele
-$database = "if20_gaspar_lu_1";
+require ("config.php");
+$database = "if20_gaspar_l_1u";
 
-
-function postStringData($input, $where, $values){ // global funktsioon, et submittida ükskõik mida
-
-    $values = $values * "?,"; 
-    $notice = null; //teade
-    
-    if (isset($_POST[$input])){
-        $conn = new mysqli($GLOBALS["serverhost"], $GLOBALS["serverusername"], $GLOBALS["serverpassword"], $GLOBALS["database"]);
-        // valmistan ete sql käsu
-        $stmt = $conn->prepare("INSERT INTO $where($input) VALUES ($values)");
+function addApplication($appName, $contact, $client, $url, $version, $serverAddress, $serverPlace){
+    if(isset($_POST["saveApp"])){
+        $conn = new mysqli($serverhost, $username, $password, $database);
+        $stmt = $conn->prepare("INSERT INTO App(Name, Platform, URL, Server_address, Server_place, Contact, Version, Client) VALUES(?,?,?,?,?,?,?,?)");
         echo $conn->error;
 
-        $stmt->bind_param("ssi", $where, $input, $values);
-
-        if($stmt->execute()){
-            $notice = "data submitted";
-        } else{
-            $notice = $stmt->error;
-        }
-
+        $stmt->bind_param("ssssssss",$appName, $contact, $client, $url, $version, $serverAddress, $serverPlace );
+        $stmt->execute();
         $stmt->close();
         $conn->close();
-        return $notice;
+
     }
-
-    function postIntData($input, $where, $values){ // Integer väärtuste submittimine
-
-        $values = $values * "?,"; 
-        $notice = null; //teade
-        
-        if (isset($_POST[$input])){
-            $conn = new mysqli($GLOBALS["serverhost"], $GLOBALS["serverusername"], $GLOBALS["serverpassword"], $GLOBALS["database"]);
-            // valmistan ete sql käsu
-            $stmt = $conn->prepare("INSERT INTO $where($input) VALUES ($values)");
-            echo $conn->error;
-    
-            $stmt->bind_param("sii", $where, $input, $values);
-    
-            if($stmt->execute()){
-                $notice = "data submitted";
-            } else{
-                $notice = $stmt->error;
-            }
-    
-            $stmt->close();
-            $conn->close();
-            return $notice;
-        }
-    }
-
 }
+
+
+
+// function postStringData($input, $where){ // global funktsioon, et submittida ükskõik mida
+//     $notice = null; //teade
+    
+//     if (isset($_POST[$input])){
+//         $conn = new mysqli($GLOBALS["serverhost"], $GLOBALS["serverusername"], $GLOBALS["serverpassword"], $GLOBALS["database"]);
+//         // valmistan ete sql käsu
+//         $stmt = $conn->prepare("INSERT INTO $where($input) VALUES (?)");
+//         echo $conn->error;
+
+//         $stmt->bind_param("ss", $where, $input);
+
+//         if($stmt->execute()){
+//             $notice = "data submitted";
+//         } else{
+//             $notice = $stmt->error;
+//         }
+
+//         $stmt->close();
+//         $conn->close();
+//         return $notice;
+//     }
+
+
 ?>
--- home.php
+++ home.php
@@ -2,10 +2,13 @@
 require ("header.php"); //basic html algus utf estiga jne
 require ("fnc_add.php"); // basic post funktsioon
 
-postStringData($_POST["name"], "App", 3);
-postStringData($_POST["contact"], "App", 3);
-postStringData($_POST["client"], "App", 3);
-postStringData($_POST["url"], "App", 3)
+$appName = $_POST["appName"];
+$contact = $_POST["contact"];
+$client = $_POST["client"];
+$url = $_POST["url"];
+$version = $_POST["version"];
+$serverAddress = $_POST["serverAddress"];
+$serverPlace = $_POST["serverPlace"];
 
 
 //php lõpp
@@ -16,7 +19,7 @@ postStringData($_POST["url"], "App", 3)
   
   <form method="POST">
 	<label for="name">Nimetus</label>
-	<input type="text" name="name" id="name" placeholder="Nimetus" required>
+	<input type="text" name="appName" id="appName" placeholder="Nimetus" required>
 	<br>
 	<label for="contact">Vastutav isik</label>
 	<input type="text" name="contact" id="contact" placeholder = "Kes vastutab?" required>
@@ -24,17 +27,15 @@ postStringData($_POST["url"], "App", 3)
 	<label for="client">Klient</label>
 	<input type="text" name="client" id="client" placeholder="Lavastaja nimi" required>
 	<br>
-	<label for="url">URL</label>
-	<input type="text" name="url" id="url" value="url" required> <!peaks muutma type text to link? >
+	<p>URL</p><input type="text" name="url" id="url" value="url" required> <!peaks muutma type text to link? >
 	<br>
-	<label for="version">Versioon</label>
-	<input type="char" name="version" id="version" placeholder="Versioon">
+	<p>Versioon/p><input type="text" name="version" id="version" placeholder="Versioon">
 	<br>
-	<label for="serverAddress">Rakenduse serveri aadress</label>
-	<input type="text" name="serverAdress" id="serverAddress" placeholder="Serveri aadress" required>
+	<p>Serveri aadress</p><input type="text" name="serverAddress" id="serverAddress" placeholder="Serveri aadress" required>
 	<br>
-
-	<input type="submit" name="save" value="Lisa rakendus">
+	<p>Serveri asukoht</p><input type="text" name="serverPlace" id="serverPlace" placeholder="Serveri asukoht" required>
+	<br>
+	<input type="submit" name="saveApp" value="Lisa rakendus">
   </form>
 </body>
 </html>
