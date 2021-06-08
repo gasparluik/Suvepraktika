eb4802ac6969cdf2247a61e73c087adfc654c38d
Gaspar Luik <luik.gaspar@gmail.com>
2021-06-08 11:14:30 +0300

    Funkstionaalsus olemas
    lisada veel kujundus

--- add_app.php
+++ add_app.php
@@ -1,85 +1,41 @@
 <?php
-//require ("fnc_add.php"); // basic post funktsioon
+require("config.php"); //andmebaasi andmed
 
+$conn = mysqli_connect($serverhost, $serverusername, $serverpassword, $database); // ühendust andmebaasiga
 
+if( isset($_POST['submitApp'])){ //kui vajutatakse submiti...
+    if(!empty($_POST['name']) && !empty($_POST['contact']) && !empty($_POST['url']) && !empty($_POST['platform']) && !empty($_POST['client']) &&!empty($_POST['serverAddress']) &&!empty($_POST['serverPlace'])){
+        $name = $_POST["name"];
+        $contact = $_POST["contact"];
+        $client = $_POST["client"];
+        $url = $_POST["url"];
+        $version = $_POST["version"];
+        $serverAddress = $_POST["serverAddress"];
+        $serverPlace = $_POST["serverPlace"];
+        $comment = $_POST["comment"];
+        $platform = $_POST["platform"];
 
-$inputerror = "";
-//kui klikiti submit, siis ...
-if(isset($_POST["submitApp"])){
-  if(empty($_POST["appName"]) or empty($_POST["contact"]) or empty($_POST["url"]) or empty($_POST["serverPlace"])){
-	$inputerror .= "Osa infot on sisestamata! ";
-  } else {
-	$appName = $_POST["appName"];
-	$contact = $_POST["contact"];
-	$client = $_POST["client"];
-	$url = $_POST["url"];
-	$version = $_POST["version"];
-	$serverAddress = $_POST["serverAddress"];
-	$serverPlace = $_POST["serverPlace"];
-	$commentInput = $_POST["commentInput"];
-	$platform = $_POST["platform"];
+        $query = "INSERT INTO App (Name, Platform, URL, Server_address, Server_place, Contact, Version, Comments, Client) VALUES('$name', '$platform', '$url', '$serverAddress', '$serverPlace', '$contact', '$version', '$comment', '$client')"; //lause andmete sisestamiseksz
+        $run = mysqli_query($conn, $query) or die (mysqli_error($conn)); //lisa andmed andmebaasi
 
-	//addApplication($_POST["appName"], $_POST["platform"], $_POST["contact"], $_POST["client"], $_POST["url"], $_POST["version"], $_POST["serverAddress"], $_POST["serverPlace"], $_POST["commentInput"]);
-	 $database = 'if20_gaspar_l_1u';
-	 $serverhost = 'localhost';
-	 $serverusername = 'if20';
-	 $serverpassword = 'if20';
-	 $conn = new mysqli($serverhost, $serverusername, $serverpassword, $database);
-
-	//$conn = new mysqli($GLOBALS["serverhost"], $serverusername, $serverpassword, $database); //connection to mySql
-
-	 $notice = null;
-	 if ($conn->connection_error){
-		 die("Connection failed to database:" . $conn->connection_error);
-	 }
-	 $stmt = $conn->prepare("INSERT INTO App (Name, Platform, URL, Server_address, Server_place, Contact, Version, Comments, Client) VALUES()");
-	 echo $conn->error;
-	 $stmt->bind_param("sssssssss", $appName, $platform, $url, $serverAddress, $serverPlace, $contact, $version, $commentInput, $client);
-	 $stmt->execute();
-	 if($stmt->execute()){
-		 $notice = "kõik on ok";
-	 } else {
-	 	$notice = $stmt->error;
-	 }
-
-	 $stmt->close();
-	 $conn->close();
-	 return $notice;
-	//addApplication lõppeb
-   }
+        if($run){
+            echo "Data submitted successfully";
+        } else{
+            echo "and error occurred";
+        }
+    } else{
+        echo "all fields are required!";
+    }
 }
 
-/*
-//home.php sisesed muutujad
-$inputerror ="";
-//Funktsiooni sees kasutatavad POST muutujad
-
-
-
-//fnc_add funkstioonid
-$comment = readComment(); //loeb kommentaare
-*/
-
-/*
-//Kontrolli, et salvestamisel, ei oleks nõutud välja tühjad
-if(isset($_POST["submitApp"])){
-	if(empty($appName or $contact or $client or $url or $serverAddress or $serverPlace)){
-		$inputerror .= "Väli on tühi!";
-	} else{
-		addApplication($_POST["appName"],$_POST["platform"], $_POST["contact"], $_POST["client"], $_POST["url"],$_POST["version"], $_POST["serverAddress"],$_POST["serverPlace"],$_POST["commentInput"]);
-	}
-}
-
-require ("header.php"); //basic html algus utf estiga jne */
-//php lõpp
+require("header.php");
 ?> 
   <h1>Monitory rakenduse lisamine</h1>
 
   <hr>
-  
   <form method="POST">
 	<label for="name">Nimetus</label>
-	<input type="text" name="appName" id="appName" placeholder="Nimetus" required>
+	<input type="text" name="name" id="name" placeholder="Nimetus" required>
 	<br>
 	<label for="contact">Vastutav isik</label>
 	<input type="text" name="contact" id="contact" placeholder = "Kes vastutab?" required>
@@ -87,24 +43,25 @@ require ("header.php"); //basic html algus utf estiga jne */
 	<label for="client">Klient</label>
 	<input type="text" name="client" id="client" placeholder="Kliendi nimi" required>
 	<br>
-	<p>Platvorm</p><input type="text" name="platform" id="platform" placeholder="Platvorm.." required>
-	<p>URL</p><input type="text" name="url" id="url" placeholder = "sisesta url" required> <!peaks muutma type text to link? >
+    <label for="platform">Platvorm</label>
+	<input type="text" name="platform" id="platform" placeholder="Platvorm.." required>
+    <br>
+    <label for="url">URL</label>
+	<input type="text" name="url" id="url" placeholder = "example.ee" required>
 	<br>
-	<p>Versioon</p><input type="text" name="version" id="version" placeholder="Versioon">
+    <label for="version">Versioon</label>
+    <input type="text" name="version" id="version" placeholder="Versioon">
 	<br>
-	<p>Serveri aadress</p><input type="text" name="serverAddress" id="serverAddress" placeholder="Serveri aadress" required>
+    <label for="serverAddress">Serveri aadress</label>
+    <input type="text" name="serverAddress" id="serverAddress" placeholder="Serveri aadress" required>
 	<br>
-	<p>Serveri asukoht</p><input type="text" name="serverPlace" id="serverPlace" placeholder="Serveri asukoht" required>
+    <label for="serverPlace">Serveri asukoht</label>
+    <input type="text" name="serverPlace" id="serverPlace" placeholder="Serveri asukoht" required>
 	<br>
-	<textarea rows="10" cols="80" name="commentInput" id="commentInput" placeholder="Kommentaarid....."></textarea>
+    <label for="comment">Kommentaarid</label>
+	<textarea rows="10" cols="80" name="comment" id="comment" placeholder="Kommentaarid....."></textarea>
 	<br>
 	<input type="submit" name="submitApp" value="Lisa rakendus">
   </form>
-  <p><?php
-  echo $inputerror;
-  //echo $submit = addApplication($_POST["appName"],$_POST["platform"], $_POST["contact"], $_POST["client"], $_POST["url"],$_POST["version"], $_POST["serverAddress"],$_POST["serverPlace"],$_POST["commentInput"]);
-  ?></p>
-
-
 </body>
 </html>
--- header.php
+++ header.php
@@ -1,15 +1,7 @@
-
-<!DOCTYPE html> 
-<! Et igal lehel oleks sama algus ja lihtsustada HTML lehe tegemist > 
+<!DOCTYPE html>
 <link rel="stylesheet" type="text/css" href="universalCSS.css">
-<html lang="et">
-<head>
-	<meta charset="utf-8"/>
-	<title>Beep..Working..Beep</title>
-	
-</head>
-<body > <!style="background-color: #AED6F1;">
-<br>
+<body>
+    <br>
     <div id="logoimage">
         <image src="logotest.png" alt="logo" height="100px" width="320px">
     </div>
@@ -18,6 +10,6 @@
         <div id="container_one"></div>
         <div id="container_two">
             <div id="content">
-            </div>
-        </div>
-    </div>
+            <div id="titlecontainer">
+                    <h2 id="title"><?php echo "something"?></h2>
+                </div>
