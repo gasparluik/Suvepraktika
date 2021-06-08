226f5539f235ccad0153fad64830336a47507b92
Gaspar Luik <luik.gaspar@gmail.com>
2021-06-08 14:42:45 +0300

    First commit

---
+++ detailed_view/config.php
@@ -0,0 +1,7 @@
+<?php
+$database = 'if20_gaspar_l_1u';
+$serverhost = 'localhost';
+$serverusername = 'if20';
+$serverpassword = 'if20';
+
+$conn = mysqli_connect($serverhost, $serverusername, $serverpassword, $database); //ühendus andmebaasiga
---
+++ detailed_view/fetch.php
@@ -0,0 +1,44 @@
+<?php
+require("config.php");
+
+$output = '';
+$sql = "SELECT * FROM App WHERE name LIKE '%".$_POST["search"]."%'";
+$result = mysqli_query($connect, $sql);
+if(mysqli_num_rows($result) > 0){
+    $output .= '<h4 align = "center">Search Result</h4>';
+    $output .= '<div class = "table-responsive">
+                    <table class = "table table bordered">
+                    <tr>
+                        <th>name</th>
+                        <th>platform</th>
+                        <th>url</th>
+                        <th>server_address</th>
+                        <th>server_place</th>
+                        <th>contact</th>
+                        <th>version</th>
+                        <th>comments</th>
+                        <th>client</th>
+                        <th>date</th>
+                    </tr>';
+    while($row = mysqli_fetch_array($result)){
+        $output .= '
+            <tr>
+                <td>'.$row["name"].'</td>
+                <td>'.$row["platform"].'</td>
+                <td>'.$row["url"].'</td>
+                <td>'.$row["server_address"].'</td>
+                <td>'.$row["server_place"].'</td>
+                <td>'.$row["contact"].'</td>
+                <td>'.$row["version"].'</td>
+                <td>'.$row["comments"].'</td>
+                <td>'.$row["client"].'</td>
+                <td>'.$row["date"].'</td>
+            </tr>';
+    }
+    echo $output; //näita tulemusi
+
+} else {
+    echo "Data missing";
+};
+
+?>
