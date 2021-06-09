959b33743c398e626af5f4df6b9823968ae15c2b
Lisanne Järv <lisannejarv@gmail.com>
2021-06-09 09:55:43 +0300

    login only

--- config.php
+++ config.php
@@ -4,8 +4,5 @@
   define('db_pass', 'if20');
   define('db_name', 'if20_lisanne');
 
-  define('db_host1', 'localhost');
-  define('db_user1', 'if20');
-  define('db_pass1', 'if20');
-  define('db_name1', 'if20_forgot-password-reset');
+
 ?>
