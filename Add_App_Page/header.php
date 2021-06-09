7a3e8c596470b8e58f59218a62a6730a76506328
Gaspar Luik <luik.gaspar@gmail.com>
2021-06-09 11:50:23 +0300

    css layout added

--- Add_App_Page/header.php
+++ Add_App_Page/header.php
@@ -1,5 +1,5 @@
 <!DOCTYPE html>
-<link rel="stylesheet" type="text/css" href="universalCSS.css">
+<link rel="stylesheet" type="text/css" href="layout.css">
 <body>
     <br>
     <div id="logoimage">
@@ -7,9 +7,23 @@
     </div>
     <br>
     <div id="container"> 
-        <div id="container_one"></div>
-        <div id="container_two">
-            <div id="content">
-            <div id="titlecontainer">
-                    <h2 id="title"><?php echo "Rakenduse lisamine"?></h2>
+        <div id="container_one">
+            <div id="top">
+                <button><a href="">Lisa rakendus</a></button>
+                <button><a href="">Teavitused</a></button>
+                <button><a href="">Detailne vaade</a></button>
+                <button><a href="">Kasutajate haldus</a></button>
+            </div>
+            <div id="bottom">
+                <div id="profile">
+                    <img id="usericon" src="usericon.png"></img>
+                    <span id="name">Test Testerino</span>
                 </div>
+                <div id="profilebutton">
+                    <button><a href="">Profiil/konto</a></button>
+                </div>
+                <button id="logout"><a href=""a>Logi välja</a>
+            </div>
+        </div>
+        <div id="container_two"></div>
+    </div>
